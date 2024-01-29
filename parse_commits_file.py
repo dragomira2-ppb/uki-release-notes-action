86269b4c3d98eccdb7b95cb84f3194e87a4d1ed5
dragomira2 <alexandru.dragomir@ppb.com>
2024-01-29 13:19:50 +0200

    small modifications

--- parse_commits_file.py
+++ parse_commits_file.py
@@ -3,8 +3,8 @@ import re
 from validate_env_variables import ENV_VARIABLES
 
 def parse_commit_data():
-        with open('release.txt', 'r') as file:
-            input_release_commits = file.read()
+    with open('release.txt', 'r') as file:
+       input_release_commits = file.read()
 
     commit_data_list = []
     commit_pattern = re.compile(
--- validate_env_variables.py
+++ validate_env_variables.py
@@ -10,7 +10,6 @@ ENV_VARIABLES = {
     'ENVIRONMENT' : os.environ.get('ENVIRONMENT', ''),
 
     #release details env variables
-    'COMMIT_MESSAGE' : os.environ.get('COMMIT_MESSAGE', 'No changes between releases'),
     'RELEASE_NAME' : os.environ.get('RELEASE_NAME', ''),
     'BUILD_NUMBER' : os.environ.get('BUILD_NUMBER', ''),
 
