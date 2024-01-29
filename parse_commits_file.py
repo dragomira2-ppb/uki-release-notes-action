998acb04271a14cff4761f05f220be81fddd6a94
dragomira2 <alexandru.dragomir@ppb.com>
2024-01-29 13:50:18 +0200

    small modifications

--- parse_commits_file.py
+++ parse_commits_file.py
@@ -3,7 +3,8 @@ import re
 from validate_env_variables import ENV_VARIABLES
 
 def parse_commit_data():
-    input_release_commits = ENV_VARIABLES['COMMIT_MESSAGE']
+    input_release_commits = ENV_VARIABLES['COMMIT_MESSAGE'] # has the -n instead of new line character
+    input_release_commits = input_release_commits.replace('-n', '\n')
 
     commit_data_list = []
     commit_pattern = re.compile(
