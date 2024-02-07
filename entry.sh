b829945d536e32b925fd80d7b8d968d9fd272147
dragomira2 <alexandru.dragomir@ppb.com>
2024-02-07 15:53:53 +0200

    modification to action yaml

--- entry.sh
+++ entry.sh
@@ -41,12 +41,10 @@ release_message=$(git log --pretty=medium "$old_tag".."$new_tag" | tr '\n' '\n')
 
 if [  -z "${release_message}"  ] || [ "${release_message}" == ""  ]; then
   echo "release_message<<$EOF" >> "$GITHUB_OUTPUT"
-  printf "This release uses a published package with version $appVersion '\n' '\n'" >> "$GITHUB_OUTPUT"
-  echo "No new changes between old tag ${old_tag} and new tag ${new_tag}." >> "$GITHUB_OUTPUT"
+  printf "This release uses a published package with version $appVersion '\n' '\n' No new changes between old tag ${old_tag} and new tag ${new_tag}." >> "$GITHUB_OUTPUT"
   echo "$EOF" >> "$GITHUB_OUTPUT"
 else
   echo "release_message<<$EOF" >> "$GITHUB_OUTPUT"
-  echo "This release uses a published package with version $appVersion '\n' '\n'" >> "$GITHUB_OUTPUT"
-  git log --pretty=medium "$old_tag".."$new_tag" | tr '\n' '\n' >> "$GITHUB_OUTPUT"
+  printf "This release uses a published package with version $appVersion '\n' '\n' $(git log --pretty=medium $old_tag..$new_tag | tr '\n' '\n')" >> "$GITHUB_OUTPUT"
   echo "$EOF" >> "$GITHUB_OUTPUT"
 fi
